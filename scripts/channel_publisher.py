9b7b38320bd80903c2fe5ecad7652dddc93425e0
Minhaj6 <minhaj.sixbyte@gmail.com>
2022-09-15 13:59:12 +0600

    housekeeping

--- scripts/channel_publisher.py
+++ scripts/channel_publisher.py
@@ -1,5 +1,4 @@
-from geometry_msgs.msg import Twist, TwistStamped
-from std_msgs.msg import Header
+from geometry_msgs.msg import Twist
 from mavros_msgs.msg import RCIn
 import rospy
 import sys, time
@@ -9,21 +8,11 @@ rospy.init_node('mhr_rc_node')
 rate = rospy.Rate(10)    # 10 Hz
 
 vel_msg = Twist()
-data_head = Header()
-vel_stamped = TwistStamped
-
-data_head.seq = 0
 
 def sub_callback(data):
-    data_head.seq += 1
-    data_head.stamp = rospy.Time.now()
-    data_head.frame_id = "base_link"
-    
     vel_msg.linear.x = data.channels[2]   # up down of right gimbal
-    vel_msg.linear.z = data.channels[0]   # left right of right gimbal
-
-    vel_stamped.header = data_head
-#    vel_stamped.twist = vel_msg
+    vel_msg.linear.y = data.channels[0]   # left right steering
+    vel_msg.linear.z = data.channels[5]   # up down of stepper (3 pos switch)
 
     rospy.loginfo(rospy.get_caller_id() + "Publishing velocity...")
 
--- scripts/pub_launch.sh
+++ scripts/pub_launch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 /usr/bin/python3 /home/ubuntu/mhrover_ws/src/channel_publisher/scripts/channel_publisher.py
